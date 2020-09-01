6a2797c124091a56ddb94acfdc5b6f947203a8ba
Moises serrano <moycs777@gmail.com>
2020-08-31 22:41:19 -0300

    stilizing request service

--- src/app/modules/services-request/components/services/services.component.ts
+++ src/app/modules/services-request/components/services/services.component.ts
@@ -1,16 +1,28 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
 
 @Component({
-  selector: 'app-services',
-  templateUrl: './services.component.html',
-  styleUrls: ['./services.component.scss']
+  selector: "app-services",
+  templateUrl: "./services.component.html",
+  styleUrls: ["./services.component.scss"],
 })
 export class ServicesComponent implements OnInit {
   @Input() name: string;
+  public serv_res = "serv_res.svg";
+  public serv_emp = "serv_emp.svg";
+  public serv_ded = "serv_ded.svg";
+  public icon = "";
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    if (this.name == "Residencial") {
+      this.icon = this.serv_res;
+    }
+    if (this.name == "Empresarial") {
+      this.icon = this.serv_emp;
+    }
+    if (this.name == "Dedicado") {
+      this.icon = this.serv_ded;
+    }
   }
-
 }
