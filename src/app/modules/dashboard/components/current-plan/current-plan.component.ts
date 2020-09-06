9b3d7d0cda9bb5a8ec34dc27a2ba14e319e77d23
Moises serrano <moycs777@gmail.com>
2020-09-05 22:47:13 -0300

    css for my plan info

--- src/app/modules/dashboard/components/current-plan/current-plan.component.ts
+++ src/app/modules/dashboard/components/current-plan/current-plan.component.ts
@@ -1,16 +1,59 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { RequestApiService } from "src/app/services/request-api.service";
+import { SessionsClientService } from "src/app/services/sessions-client.service";
+import { UtilsService } from "src/app/services/utils.service";
 
 @Component({
-  selector: 'app-current-plan',
-  templateUrl: './current-plan.component.html',
-  styleUrls: ['./current-plan.component.scss']
+  selector: "app-current-plan",
+  templateUrl: "./current-plan.component.html",
+  styleUrls: ["./current-plan.component.scss"],
 })
 export class CurrentPlanComponent implements OnInit {
-
   public plan;
-  constructor() { }
+  public cliente;
+  public solicitation: any = null;
+  public contracts: any = [];
+
+
+  constructor(
+    private sessions: SessionsClientService,
+    private http: RequestApiService,
+    private utils: UtilsService
+  ) {}
 
   ngOnInit(): void {
+    let session: any = this.sessions.getCurrentSession();
+    if (session.es_cliente) {
+      this.getContractStatus();
+    } else {
+      this.getSolicitationStatus();
+    }
   }
 
+  async getContractStatus() {
+    this.http.get("admon/contratos-status", null, true).subscribe(
+      (response: any) => {
+        console.log("response, getContractStatus", response);
+        this.contracts = response.results;
+        console.log(this.contracts);
+      },
+      (error) => {
+        console.log(error.error.message);
+        console.log(error);
+      }
+    );
+  }
+
+  async getSolicitationStatus() {
+    this.http.get("admon/solicitud-status", null, true).subscribe(
+      (response: any) => {
+        console.log("response, getSolicitationStatus", response);
+        this.solicitation = response;
+      },
+      (error) => {
+        console.log(error.error.message);
+        console.log(error);
+      }
+    );
+  }
 }
