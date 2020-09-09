8bb5f0700bbf9e24750980bec7c28033f808d4b4
Moises serrano <moycs777@gmail.com>
2020-09-08 21:28:59 -0300

    bills

--- src/app/modules/dashboard/components/bill-item-box/bill-item-box.component.ts
+++ src/app/modules/dashboard/components/bill-item-box/bill-item-box.component.ts
@@ -1,20 +1,26 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input } from "@angular/core";
 
 @Component({
-  selector: 'app-bill-item-box',
-  templateUrl: './bill-item-box.component.html',
-  styleUrls: ['./bill-item-box.component.scss']
+  selector: "app-bill-item-box",
+  templateUrl: "./bill-item-box.component.html",
+  styleUrls: ["./bill-item-box.component.scss"],
 })
 export class BillItemBoxComponent implements OnInit {
-
   @Input() id;
+  @Input() index;
   @Input() status;
   @Input() date;
   @Input() bolivar;
   @Input() dolar;
-  constructor() { }
+  public isBgWhite = true;
+
+  constructor() {}
 
   ngOnInit(): void {
+    // console.log("index", this.index);
+    if (this.index % 2 !== 0) {
+      console.log("this.index", this.index, this.index % 2);
+      this.isBgWhite = false;
+    }
   }
-
 }
