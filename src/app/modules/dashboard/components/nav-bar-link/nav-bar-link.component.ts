a5c1ad1b0c02ad94d194782a929a85e93291b564
Moises serrano <moycs777@gmail.com>
2020-09-11 22:11:50 -0300

    iconos

--- src/app/modules/dashboard/components/nav-bar-link/nav-bar-link.component.ts
+++ src/app/modules/dashboard/components/nav-bar-link/nav-bar-link.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit, Input } from "@angular/core";
 })
 export class NavBarLinkComponent implements OnInit {
   @Input() name;
+  @Input() icon;
   @Input() iconPosition = "left";
   constructor() {}
 
