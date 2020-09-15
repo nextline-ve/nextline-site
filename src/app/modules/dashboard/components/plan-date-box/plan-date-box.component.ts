36d7caaedf00fb9fa6f5e9b1c17d896c76637aab
Moises serrano <moycs777@gmail.com>
2020-09-15 08:28:12 -0300

    configuring pay day

--- src/app/modules/dashboard/components/plan-date-box/plan-date-box.component.ts
+++ src/app/modules/dashboard/components/plan-date-box/plan-date-box.component.ts
@@ -1,16 +1,34 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input } from "@angular/core";
+import * as moment from "moment";
 
 @Component({
-  selector: 'app-plan-date-box',
-  templateUrl: './plan-date-box.component.html',
-  styleUrls: ['./plan-date-box.component.scss']
+  selector: "app-plan-date-box",
+  templateUrl: "./plan-date-box.component.html",
+  styleUrls: ["./plan-date-box.component.scss"],
 })
 export class PlanDateBoxComponent implements OnInit {
+  @Input() date = null;
+  public dateFormated;
 
-  @Input() date;
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    setTimeout(() => {
+      this.calculateDate();
+    }, 1000);
   }
 
+  ngAfterContentInit() {}
+
+  calculateDate() {
+    const now = moment();
+    const year = now.get("year");
+    const month = now.get("month");
+    const day = now.get("day");
+    const currentMonth = this.date >= day ? 2 : 1;
+
+    const planDate = `${this.date}-${month + currentMonth}-${year}`;
+    console.log(planDate);
+    this.dateFormated = moment(planDate, "DD-MM-YYYY").format("DD/MM/YYYY");
+  }
 }
