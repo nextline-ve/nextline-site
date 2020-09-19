90e2c7433ce430d6f3c4e4cadfbed0d4a83ed1e9
Moises serrano <moycs777@gmail.com>
2020-09-19 20:10:39 -0300

    payment day

--- src/app/modules/dashboard/pages/plan-detail/plan-detail.component.ts
+++ src/app/modules/dashboard/pages/plan-detail/plan-detail.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute, Router } from "@angular/router";
 import { RequestApiService } from "src/app/services/request-api.service";
 import { UtilsService } from "src/app/services/utils.service";
+import * as moment from "moment";
 
 @Component({
   selector: "app-plan-detail",
@@ -12,6 +13,7 @@ export class PlanDetailComponent implements OnInit {
   public plan: any = {};
   public cliente: any = {};
   public isLoading = false;
+  public dateFormated = null;
 
   constructor(
     private route: ActivatedRoute,
@@ -24,11 +26,18 @@ export class PlanDetailComponent implements OnInit {
     this.route.queryParams.subscribe((res: any) => {
       console.log(res);
       this.plan = res;
+      // to do
       // this.formatDate();
     });
     const user: any = await localStorage.getItem("nextline-currentClient");
     this.cliente = JSON.parse(user);
     console.log("this.cliente", this.cliente);
+  }
+
+  formatDate() {
+    this.dateFormated = moment(this.plan.fecha_creacion, "YYYY-MM-DD").format(
+      "DD/MM/YYYY"
+    );
   }
 
   comfirm() {
