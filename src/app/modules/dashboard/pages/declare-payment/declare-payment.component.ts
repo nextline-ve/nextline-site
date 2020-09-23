a28fd9f0a7845eec65ed4f65623f74138a22467d
Moises serrano <moycs777@gmail.com>
2020-09-23 20:52:46 -0300

    structuring declare payment

--- src/app/modules/dashboard/pages/declare-payment/declare-payment.component.ts
+++ src/app/modules/dashboard/pages/declare-payment/declare-payment.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { environment } from "src/environments/environment.prod";
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from "@angular/forms";
+import { UtilsService } from "src/app/services/utils.service";
+import { RequestApiService } from "src/app/services/request-api.service";
+import { SessionsClientService } from "src/app/services/sessions-client.service";
 
 @Component({
-  selector: 'app-declare-payment',
-  templateUrl: './declare-payment.component.html',
-  styleUrls: ['./declare-payment.component.scss']
+  selector: "app-declare-payment",
+  templateUrl: "./declare-payment.component.html",
+  styleUrls: ["./declare-payment.component.scss"],
 })
 export class DeclarePaymentComponent implements OnInit {
+  public myForm: FormGroup;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private session: SessionsClientService,
+    private http: RequestApiService,
+    private utils: UtilsService
+  ) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
+  onFileChanged(e) {}
 }
--- src/app/modules/dashboard/pages/select-payment-type/select-payment-type.component.ts
+++ src/app/modules/dashboard/pages/select-payment-type/select-payment-type.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-import { ActivatedRoute } from "@angular/router";
+import { ActivatedRoute, Router } from "@angular/router";
 
 @Component({
   selector: "app-select-payment-type",
@@ -11,13 +11,21 @@ export class SelectPaymentTypeComponent implements OnInit {
     { id: 1, name: "Zelle", icon: "zelle.png" },
     { id: 1, name: "Bank of america", icon: "bofa.png" },
   ];
-  constructor(private route: ActivatedRoute) {}
+  public bill: any = {};
+
+  constructor(private route: ActivatedRoute, private router: Router) {}
 
   ngOnInit(): void {
     this.route.queryParams.subscribe((res: any) => {
       console.log(res);
       // this.plan = res;
       // this.getContractStatus();
+    });
+  }
+
+  selectPayment(item) {
+    this.router.navigate(["/panel/bills/declare-payment"], {
+      queryParams: { payment: item, bill: this.bill },
     });
   }
 }
