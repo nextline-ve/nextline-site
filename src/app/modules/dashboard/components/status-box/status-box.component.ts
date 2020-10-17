006cc50eec1bfb4fa07803eef9bc3a08754ce6b3
Moises serrano <moycs777@gmail.com>
2020-10-17 13:08:44 -0300

    status colors

--- src/app/modules/dashboard/components/status-box/status-box.component.ts
+++ src/app/modules/dashboard/components/status-box/status-box.component.ts
@@ -13,5 +13,24 @@ export class StatusBoxComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {
+    console.warn(this.status, this.statusDetail);
   }
+
+  getColor(status) { 
+    console.warn("getColor", status);
+    
+    switch (status) {
+      case 'P':
+        return '#58e9ad 0% 0% no-repeat padding-box';
+      case 'C':
+        return '#cbe83b 0% 0% no-repeat padding-box';
+      case 'V':
+        return '#c73407 0% 0% no-repeat padding-box';
+      case 'G':
+        return '#586ad4 0% 0% no-repeat padding-box';
+      default:
+        return '#52c0f2 0% 0% no-repeat padding-box'
+    }
+  }
+  
 }
