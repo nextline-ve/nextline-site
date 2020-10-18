bb0abd6a9648c7ab47b94692f0bbd29f4006b314
abejarano <programador.angel@gmail.com>
2020-10-18 14:24:33 -0300

    fix generales

--- src/app/modules/dashboard/pages/bill-detail/bill-detail.component.ts
+++ src/app/modules/dashboard/pages/bill-detail/bill-detail.component.ts
@@ -33,7 +33,6 @@ export class BillDetailComponent implements OnInit {
 
       this.billId = res.id;
       this.bill = {...res};
-      console.warn('this.bill', this.bill);
       this.formatDate();
       this.loadCurrencies();
     });
