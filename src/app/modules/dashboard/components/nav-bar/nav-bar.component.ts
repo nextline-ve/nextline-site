14eb0e42c87f67adfff493fbac80a3ea5adf0b84
abejarano <programador.angel@gmail.com>
2020-10-17 23:24:20 -0300

    refactor code.

--- src/app/modules/dashboard/components/nav-bar/nav-bar.component.ts
+++ src/app/modules/dashboard/components/nav-bar/nav-bar.component.ts
@@ -1,23 +1,22 @@
-import { Component, OnInit } from "@angular/core";
-import { BreakpointObserver, Breakpoints } from "@angular/cdk/layout";
-import { Observable } from "rxjs";
-import { map, shareReplay } from "rxjs/operators";
-import { SessionsClientService } from "src/app/services/sessions-client.service";
-import { environment } from "src/environments/environment.prod";
-import { RequestApiService } from "src/app/services/request-api.service";
+import {Component, OnInit} from '@angular/core';
+import {BreakpointObserver, Breakpoints} from '@angular/cdk/layout';
+import {Observable} from 'rxjs';
+import {map, shareReplay} from 'rxjs/operators';
+import {SessionsClientService} from 'src/app/services/sessions-client.service';
+import {environment} from 'src/environments/environment.prod';
+import {RequestApiService} from 'src/app/services/request-api.service';
 
 @Component({
-  selector: "app-nav-bar",
-  templateUrl: "./nav-bar.component.html",
-  styleUrls: ["./nav-bar.component.scss"],
+  selector: 'app-nav-bar',
+  templateUrl: './nav-bar.component.html',
+  styleUrls: ['./nav-bar.component.scss'],
 })
 export class NavBarComponent implements OnInit {
   public domain = environment.DOMAIN;
   public cliente = {
-    nombre_razsoc: "User Full Name Optional",
+    nombre_razsoc: 'User Full Name Optional',
   };
-  public avatar =
-    "https://pbs.twimg.com/profile_images/527229878211321857/Ken4pm5u_400x400.jpeg";
+  public avatar = '../../../../../assets/images/default-avatar.jpeg';
 
   isHandset$: Observable<boolean> = this.breakpointObserver
     .observe(Breakpoints.Handset)
@@ -30,7 +29,8 @@ export class NavBarComponent implements OnInit {
     private breakpointObserver: BreakpointObserver,
     private session: SessionsClientService,
     private http: RequestApiService
-  ) {}
+  ) {
+  }
 
   ngOnInit(): void {
     const session: any = this.session.getCurrentSession();
@@ -43,35 +43,26 @@ export class NavBarComponent implements OnInit {
   }
 
   async getProfile() {
-    this.http.get("admon/perfil", null, true).subscribe(
+    this.http.get('admon/perfil', null, true).subscribe(
       (response: any) => {
         this.cliente = response;
         this.verifyAvatar(response.avatar);
       },
-      (error) => {
-        console.log(error.error.message);
-        console.log(error);
-      }
+      (error) => { }
     );
   }
 
   async getSolicitationStatus() {
-    this.http.get("admon/solicitud-status", null, true).subscribe(
+    this.http.get('admon/solicitud-status', null, true).subscribe(
       (response: any) => {
         this.cliente = response;
       },
-      (error) => {
-        console.log(error.error.message);
-        console.log(error);
-      }
+      (error) => { }
     );
   }
 
   verifyAvatar(img) {
-    if (img == null) {
-      this.avatar =
-        "../../../../../assets/images/default-avatar.jpeg";
-    } else {
+    if (img != null) {
       this.avatar = img;
     }
   }
