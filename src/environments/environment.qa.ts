a53acc433646a3f83247b774184452e3b49a0ec3
jaspesoft <130202@ngel>
2021-03-01 13:05:38 -0300

    FIX environment QA

---
+++ src/environments/environment.qa.ts
@@ -0,0 +1,29 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  GOOGLE_MAPS_KEY: "AIzaSyBMAbWtexOQ_qiuGvHXxAGp-455u8WWkOk",
+  URL_API: "https://nextline.jaspesoft.com/api/v1/",
+  DOMAIN: "https://nextline.jaspesoft.com",
+  firebaseConfig : {
+    apiKey: "AIzaSyB_SiugFuU2jYqY-DwXl7dGe91XArYzXuA",
+    authDomain: "nextline-ve.firebaseapp.com",
+    databaseURL: "https://nextline-ve.firebaseio.com",
+    projectId: "nextline-ve",
+    storageBucket: "nextline-ve.appspot.com",
+    messagingSenderId: "924710173144",
+    appId: "1:924710173144:web:fd02478cda66f468531aad",
+    measurementId: "G-LBFFS64X43"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
